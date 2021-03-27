93ab72e7af2de783a470fb56821013f1440be219
heetmeyer <heerfn@yahoo.de>
2021-03-27 01:53:00 +0100

    Add capability to write dataset to str

--- src/test_predictor.py
+++ src/test_predictor.py
@@ -5,12 +5,14 @@ import numpy as np
 
 dataset = []
 labels = []
+dataset_str = ''
 
 with open('LatencyTraining.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for (iter, value) in reader:
         if iter == '':
             continue
+        dataset_str += (value + ",")
         value = float(value)
         dataset.append(value)
         if value < 0:
@@ -18,6 +20,9 @@ with open('LatencyTraining.csv', newline='') as csvfile:
         else:
             labels.append(1)
 
+# text_file = open("dataset_string.txt", "w")
+# text_file.write(dataset_str)
+# text_file.close()
 
 l = len(dataset)
 ma_length = 3
