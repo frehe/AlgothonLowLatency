ae86fcfed6e4c9d2d547aa4889ed4a17f69606a9
heetmeyer <heerfn@yahoo.de>
2021-03-27 13:01:19 +0100

    Write tester

--- src_c/main.cpp
+++ src_c/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 std::size_t AVG_LENGTH = 3; // Length over which to average
+std::size_t STRING_LENGTH = 500;
 
 int predict(std::vector<float> v)
 {
@@ -20,6 +21,32 @@ int predict(std::vector<float> v)
     {
         return 1;
     }
+}
+
+float test_predictor(std::vector<float> v)
+{
+    size_t SIZE = v.size();
+    std::vector<int> preds;
+    std::vector<int> labels;
+    float accuracy = 0;
+
+    for (uint16_t i = 0; i < SIZE - AVG_LENGTH; i++)
+    {
+        std::vector<float> slice;
+        for (uint16_t k = 0; k < AVG_LENGTH; k++) {
+            slice.push_back(v.at(i+k));
+        }
+        preds.push_back(predict(slice));
+        if (v.at(i+AVG_LENGTH) < 0)
+            labels.push_back(0);
+        else
+            labels.push_back(1);
+        if (preds.back() == labels.back())
+            accuracy += 1;
+    }
+
+    accuracy /= (SIZE - AVG_LENGTH);
+    return accuracy;
 }
 
 int main()
@@ -39,6 +66,9 @@ int main()
                 stream.ignore();
             }
         }
+        // Test predictor on all subsets of size AVG_LENGTH
+        // float accuracy = test_predictor(v);
+        // std::cout << "Accuracy on test: " << accuracy << std::endl;
         // Print prediction
         std::cout << predict(v) << std::endl;
     }
