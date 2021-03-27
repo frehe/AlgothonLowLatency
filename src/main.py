cf6b13533d917d897edd919b9a9a1d01a67831b1
heetmeyer <heerfn@yahoo.de>
2021-03-27 01:05:03 +0100

    Add cython-powered MA

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
+import cython
+import predict
 from sys import stdin
 
-WINDOWSIZE=500 # windowsize input 
 
 for line in stdin:
     if line == '': 
         break
     d=[float(x) for x in line.split(',')]
-    print('0')  # 0 or 1
+    prediction = predict.predict(d)
+    print(str(prediction)))  # 0 or 1
