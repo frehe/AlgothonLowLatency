0831c4f49766973fd6d7ad6fe6d46bb1fcbc643c
heetmeyer <heerfn@yahoo.de>
2021-03-27 02:22:07 +0100

    Changes

--- src/test_predictor.py
+++ src/test_predictor.py
@@ -1,6 +1,6 @@
 from sys import stdin
 import csv
-import predict
+from predict import predict
 import numpy as np
 
 dataset = []
@@ -31,8 +31,8 @@ current_acc = 0
 # Dataset has 1826 values. Do time series predictions on it:
 for i in range(ma_length, l):
     data_subset = dataset[i-ma_length:i]
-    preds.append(predict.predict(data_subset))
-    # print(np.average(np.equal(preds[ma_length:], labels[ma_length:len(preds)])))
+    preds.append(predict(data_subset))
+    print(np.average(np.equal(preds[ma_length:], labels[ma_length:len(preds)])))
 
 accuracy = np.average(np.equal(preds[ma_length:], labels[ma_length:]))
 print(accuracy)
